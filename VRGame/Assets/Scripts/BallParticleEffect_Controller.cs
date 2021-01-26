2de39f1681ab03069213bb852543e62a2db41cd6
MaxRim3 <maximrioumine@hotmail.com>
2021-01-26 22:35:57 +1100

    added new balls, some effects, playing around with sound, increased reach

---
+++ VRGame/Assets/Scripts/BallParticleEffect_Controller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallParticleEffect_Controller : MonoBehaviour
+{
+    public GameObject particleSystemPrefab;
+    public bool hit;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        float particleScale = collision.transform.gameObject.GetComponent<Rigidbody>().velocity.x + collision.transform.gameObject.GetComponent<Rigidbody>().velocity.y + collision.transform.gameObject.GetComponent<Rigidbody>().velocity.z;
+        ContactPoint contact = collision.contacts[0];
+        
+        GameObject hitParticle = Instantiate(particleSystemPrefab, contact.point, Quaternion.identity) as GameObject;
+        hitParticle.transform.localScale = new Vector3(particleScale / 2, particleScale / 2, particleScale / 2);
+
+    }
+}
---
+++ VRGame/Assets/Scripts/BallSoundEffect_Controller.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSoundEffect_Controller : MonoBehaviour
+{
+    public AudioClip[] audioClips;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public AudioSource audioSource;
+    public AudioSource audioSourceSecondary;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Racket")
+        {
+           
+            float collisionNum = collision.gameObject.GetComponent<Rigidbody>().velocity.x + collision.gameObject.GetComponent<Rigidbody>().velocity.y + collision.gameObject.GetComponent<Rigidbody>().velocity.z;
+            //if (!audioSource.isPlaying)
+            {
+                //audioSource.volume = collisionNum / 10;
+                //audioSourceSecondary.volume = collisionNum / 10;
+                //audioSource.clip = audioClips[0];
+                audioSource.Play();
+            }
+            //if (!audioSourceSecondary.isPlaying)
+            {
+                audioSourceSecondary.Play();
+            }
+        }
+        else
+        {
+            audioSourceSecondary.volume = 0.3f;
+            audioSourceSecondary.Play();
+        }
+
+    }
+}
